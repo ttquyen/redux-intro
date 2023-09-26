3a2e8bd93141e88b45443b08e26e9a559b0acb4f
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-09-26 22:38:47 +0700

    🐛 remove warning

--- src/service/cart/slice.js
+++ src/service/cart/slice.js
@@ -32,6 +32,6 @@ export const cartSlice = createSlice({
     },
   },
 });
-const { actions, reducers } = cartSlice;
+const { actions } = cartSlice;
 export const { addToCart, removeCart } = actions;
 // export default reducers
